28f51b94391c85e9f0fa8d3e7db969eb903e50e4
Alex <alaster.rom@gmail.com>
2020-01-28 20:06:57 +0200

    add 2 file / 31 line are added

---
+++ src/SimpleConsoleApp.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Example {
+	// вызов метода main().
+	public static void main(String args[]) {
+		logger.log("Простая программа на Java.");
+	}
+	
+	public static int ung(int var1, int var2) {
+        int tempRezult = var1 * var2;
+        System.out.println("Rezult = " + tempRezult);
+        return tempRezult;
+    }
+	
+	public static int min(int var1, int var2) {
+        int tempRezult = var1 - var2;
+        System.out.println("Rezult = " + tempRezult);
+        return tempRezult;
+    }
+}
