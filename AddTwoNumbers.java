f8d422f17b8bc8906d589c6affd72e9e77f3c621
Alex <alaster.rom@gmail.com>
2020-01-28 20:52:01 +0200

    file rename / add 1 line

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,8 +2,9 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5, sum;
+      int num1 = 5;
 	  int num2 = 15;
+	  int sum;
 
       sum = num1 + num2;
       
