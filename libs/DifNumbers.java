d16ac557a21d93392b03e29da29bb724c0a50565
Alex <alaster.rom@gmail.com>
2020-02-03 14:57:17 +0200

    add package, file / add 9 line

---
+++ libs/DifNumbers.java
@@ -0,0 +1,9 @@
+public class DifNumbers {
+
+   public static void main(String[] args) {
+      int num1 = 5, num2 = 15; 
+	  int dif;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+   }
+}
